3cd637acf99f1689309455740df7759a5d9eab16
GarrardFontanive <car.fontanive@gmail.com>
2026-07-06 23:38:51 -0300

    Atividade de persistencia

---
+++ persistencia/conexao.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $pdo = new PDO('sqlite:banco.sqlite');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro de conexão: " . $e->getMessage();
+    exit;
+}
+?>
